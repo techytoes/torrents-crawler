0f3972354fa50a46515d695c6c20b2fe64f56507
firedranzer <rupeshharode@gmail.com>
2017-08-20 13:05:08 +0530

    removed kickass spider

--- piratebay/piratebay/settings.py
+++ piratebay/piratebay/settings.py
@@ -13,3 +13,6 @@ NEWSPIDER_MODULE = 'piratebay.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'piratebay (+http://www.yourdomain.com)'
+#USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
